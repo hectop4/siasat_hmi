d47bc2694b903176d39ee4056fbcf962bb27f578
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2024-03-06 17:34:16 -0500

    first code: main code in the primary load to test the connection on the components

--- hardware_code/siasat_primary_load/src/main.cpp
+++ hardware_code/siasat_primary_load/src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
+#include <LoRa.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_MPU6050.h>
+#include <TinyGPSPlus.h>
+#include <strings.h>
+#include <TinyGPS++.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_BMP280 bmp;
+Adafruit_MPU6050 mpu;
+TinyGPSPlus gps;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+
+  // BMP280
+
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+  Serial.println("BMP280 Initializing OK!");
+
+  // MPU6050
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1);
+  }
+  Serial.println("MPU6050 Initializing OK!");
+
+  // GPS
+  Serial.println("GPS Initializing OK!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// Path: hardware_code/siasat_primary_load/src/LoRa.cpp
